db859ef977a693cde094aa964f6dd82ca46ea142
Shro <deagle.shroeder@gmail.com>
2014-11-21 18:33:32 -0600

    RevertTestCommit

--- NewGame/Classes/World.cs
+++ NewGame/Classes/World.cs
@@ -5,7 +5,7 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-//Test Commit
+
 namespace NewGame.Classes
 {
     public class World
